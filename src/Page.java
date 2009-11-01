4d31355900f516a752518a19c842cc97aa6454f8
ryandotsmith <this.ryansmith@gmail.com>
2009-11-01 11:53:15 -0600

    removed all of the useless classes and added dom4j

--- src/Page.java
+++ src/Page.java
@@ -1,41 +1,48 @@
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.util.Comparator;
+import java.io.File;
+import java.util.StringTokenizer;
+import org.dom4j.*;
+import org.dom4j.io.SAXReader;
+
+
+
 
 public class Page implements Comparable {
 
-	private static String searchTearm;
+	public static String searchTearm;
 	private WordList list;
-	private Scanner scanner;
-
-
-	public Page(String fileName){
-		try{
-			scanner = new Scanner(fileName,Scanner.SCANFILE);
-			//while(!scanner.atEndOfFile()){
-				String token = scanner.getNextToken();
-				
-				//list.insert(token);
-				//;
-			//}
-		}catch (FileNotFoundException e) {
-		    System.out.println(e);
-		}catch (IOException e) {
-		    System.out.println(e);
-		}
-
+	private Document document;
+	
+	public Page(String fileName) throws DocumentException{	
+	    this.list = new WordList();
+		File file = new File(fileName);
+		SAXReader xmlReader = new SAXReader();
+		this.document = xmlReader.read(file);
 	}
-	
-	
-	public WordList getList(){
-		return this.list;
-	}
-	
+	public WordList getList() 		{return this.list;}
+	public Document getDocument() 	{return this.document;}
 	public int compareTo(Object anotherPage) throws ClassCastException {
 		if (!(anotherPage instanceof Page))
 	      throw new ClassCastException("A Page object expected.");
 	    int anotherPageRank = ((Page) anotherPage).getList().count(Page.searchTearm);  
 	    return this.getList().count(Page.searchTearm) - anotherPageRank;    
 	}
+	public void treeWalk() {
+        treeWalk( this.document.getRootElement() );
+    }
+    private void treeWalk(Element element) {
+    		for ( int i = 0, size = element.nodeCount(); i < size; i++ ) {
+            Node node = element.node(i);
+            if ( node instanceof Element ) {
+                treeWalk( (Element) node );
+            }
+            else {
+            		String string = node.getText();
+            		StringTokenizer st = new StringTokenizer(string);
+            		while (st.hasMoreTokens()) {
+            		  this.list.insert(st.nextToken());
+            		}
+            }
+        }
+    }
 
 }
--- src/PageTest.java
+++ src/PageTest.java
@@ -1,4 +1,5 @@
-import java.io.FileNotFoundException;
+import org.dom4j.DocumentException;
+import org.dom4j.tree.DefaultDocument;
 import junit.framework.TestCase;
 
 
@@ -10,16 +11,38 @@ public class PageTest extends TestCase {
 		String file = "test.html";
 		testPage = new Page(file);
 	}
-
+	public void testExceptionGetsThrownOnBadFileName() throws DocumentException{
+		try{
+			Page blah = new Page("bad_file_name");
+		}catch(DocumentException e){
+			return;
+		}
+		fail("Should raise an exception");
+	}
+	public void testThatTheConstructorHasReadTheFile(){
+		testPage.treeWalk();
+		assertEquals(1,testPage.getList().count("Title"));
+	}
 	public void testPage() {
 		assertTrue(testPage instanceof Page);
 	}
-	public void testExceptionGetsThrownOnBadFileName() throws FileNotFoundException{
-		//Page blah = new Page("bad_file_name");
+	public void testGetList(){
+		assertTrue( testPage.getList() instanceof WordList);
 	}
-	
-	public void testThatTheConstructorHasReadTheFile(){
-		assertEquals(1,testPage.getList().count("Title"));
+	public void testGetDocument(){
+		assertTrue( testPage.getDocument() instanceof DefaultDocument);
+	}
+	public void testTheComparisonOfPages() throws DocumentException{
+		testPage.getList().insert("nice");
+		testPage.getList().insert("nice");
+		assertEquals(2,testPage.getList().count("nice"));
+		Page anotherTestPage = new Page("test.html");
+		anotherTestPage.getList().insert("nice");
+		assertEquals(1,anotherTestPage.getList().count("nice"));
+		Page.searchTearm = "nice";
+		assertEquals(1,testPage.compareTo(anotherTestPage));
+		assertEquals(-1,anotherTestPage.compareTo(testPage));
 	}
 
 }
+	
